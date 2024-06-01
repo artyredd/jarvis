bede21868202afb8e815c3d04884d306c3327b53
artyredd <canadianspycrab@gmail.com>
2024-06-01 07:37:21 -0400

    add plugin loading

--- Jarvis/Source/EntryPoint.c
+++ Jarvis/Source/EntryPoint.c
@@ -1,7 +1,30 @@
 #include "core/runtime.h"
+#include "core/plugins.h"
 
 int main()
 {
 	Application.Start();
 	return 0;
+}
+
+Plugin plugin;
+
+OnStart(1)
+{
+	plugin = Plugins.Load(stack_string("ExamplePlugin.dll"));
+}
+
+OnUpdate(1)
+{
+	fprintf(stdout, "\n");
+}
+
+AfterUpdate(1)
+{
+	fprintf(stdout, "\n");
+}
+
+OnClose(1)
+{
+	Plugins.Dispose(plugin);
 }
