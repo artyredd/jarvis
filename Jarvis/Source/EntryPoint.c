411856c5a6c5c450155c3d4462e03fbdb6479831
artyredd <canadianspycrab@gmail.com>
2024-06-08 23:40:20 -0400

    fix tts plugin

--- Jarvis/Source/EntryPoint.c
+++ Jarvis/Source/EntryPoint.c
@@ -8,30 +8,23 @@ int main()
 	return 0;
 }
 
-Plugin plugin;
 Plugin ttsPlugin;
 
 OnStart(1)
 {
-	plugin = Plugins.Load(stack_string("Plugin.dll"));
 	ttsPlugin = Plugins.Load(stack_string("TTSPlugin.dll"));
 }
 
-int i = 0;
-int x = 0;
 
 OnUpdate(1)
 {
-	fprintf_red(stdout, "Jarvis OnUpdate: %i\n", i++);
 }
 
 AfterUpdate(1)
 {
-	fprintf_red(stdout, "Jarvis AfterUpdate: %i\n", x++);
 }
 
 OnClose(1)
 {
-	Plugins.Dispose(plugin);
 	Plugins.Dispose(ttsPlugin);
 }
